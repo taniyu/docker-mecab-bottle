6c0ef9590c90c55a5bda6476106f1b72001b1702
Yuji Tanimoto <y.tanimoto13@gmail.com>
2017-03-06 03:29:58 +0900

    use get parameter.

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-from bottle import route, HTTPResponse, default_app
+from bottle import route, HTTPResponse, default_app, request
 import MeCab
 import json
 
-@route('/mecab-neologd/<sentence:path>')
-def mecab_neologd(sentence):
+@route('/mecab-neologd', method='GET')
+def mecab_neologd():
+    sentence = request.query.sentence
     mecab = MeCab.Tagger('-d /usr/lib/mecab/dic/mecab-ipadic-neologd')
     result = '\n'.join(mecab.parse(sentence).split('\n')[0:-2])
     r = HTTPResponse(status=302, body=json.dumps({ 'result':result }, ensure_ascii=False))
     r.set_header('Content-Type', 'application/json')
     return r
 
-@route('/mecab/<sentence:path>')
-def mecab(sentence):
+@route('/mecab', method='GET')
+def mecab():
+    sentence = request.query.sentence
     mecab = MeCab.Tagger()
     result = '\n'.join(mecab.parse(sentence).split('\n')[0:-2])
     r = HTTPResponse(status=302, body=json.dumps({ 'result':result }, ensure_ascii=False))
